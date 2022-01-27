4d615164f47581fb1d44a0718504d5e5f7332459
olga.fourkioti <olgafour1@gmail.com>
2022-01-27 16:24:40 +0000

    histo

--- run_files/histo_run.sh
+++ run_files/histo_run.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=48:05:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/siamese_k_5.out
-#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/siamese_k_5.err
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/siamese_k_5_prob_8.out
+#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/siamese_k_5_prob_8.err
 #SBATCH --partition=gpu
 
 
@@ -14,6 +14,6 @@ module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_datasets/
-python run.py --experiment_name siamese_k_5 --mode siamese --k 5 --input_shape 27 27 3 --extention bmp --data colon --prob 0.75 --data_path ColonCancer  --siam_epochs 20 --weight_file
+python run.py --experiment_name siamese_k_5_prob_8 --mode siamese --k 5 --input_shape 27 27 3 --extention bmp --data colon --prob 0.8 --data_path ColonCancer  --siam_epochs 20 --weight_file
 
 
